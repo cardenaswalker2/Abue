9c5ef0472b86d326081a8a0f260fad4fe82bcd59
Luis Desarrollador <cardenaswalker2@gmail.com>
2026-08-23 00:13:14 -0500

    Configurar enlace de ruta GPS en Cómo llegar

--- js/app.js
+++ js/app.js
@@ -7,15 +7,16 @@ const configuracionEvento = {
     hora: "8:00 PM",
     horaDetalle: "8:00 de la noche",
     lugar: "Mi residencia",
-    lat: 10.4109318,
-    lng: -75.4578974,
-    mapsUrl: "https://www.google.com/maps/@10.4109318,-75.4578974,3a,75y,57.83h,84.66t/data=!3m7!1e1!3m5!1sMq3YuDWlXfq2pm5djMQsQw!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fcb_client%3Dmaps_sv.tactile%26w%3D900%26h%3D600%26pitch%3D5.341556406466125%26panoid%3DMq3YuDWlXfq2pm5djMQsQw%26yaw%3D57.83259701595769!7i16384!8i8192?entry=ttu&g_ep=EgoyMDI2MDgxOS4wIKXMDSoASAFQAw%3D%3D",
+    lat: 10.410916338642544,
+    lng: -75.45787621584036,
+    mapsUrl: "https://www.google.com/maps/dir/?api=1&destination=10.410916338642544,-75.45787621584036",
+    streetViewUrl: "https://www.google.com/maps/@10.4109318,-75.4578974,3a,75y,57.83h,84.66t/data=!3m7!1e1!3m5!1sMq3YuDWlXfq2pm5djMQsQw!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fcb_client%3Dmaps_sv.tactile%26w%3D900%26h%3D600%26pitch%3D5.341556406466125%26panoid%3DMq3YuDWlXfq2pm5djMQsQw%26yaw%3D57.83259701595769!7i16384!8i8192?entry=ttu&g_ep=EgoyMDI2MDgxOS4wIKXMDSoASAFQAw%3D%3D",
     rutaMusica: "assets/musica.mp3"
 };
 
 const ubicacion = {
-    lat: 10.4109318,
-    lng: -75.4578974,
+    lat: 10.410916338642544,
+    lng: -75.45787621584036,
     mapsUrl: configuracionEvento.mapsUrl
 };
 
